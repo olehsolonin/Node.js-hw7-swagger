82341b5f8c9e54478c5dd473bec5408398d69767
Oleh <62122094+olehsolonin@users.noreply.github.com>
2024-10-12 12:48:42 +0300

    Initialize hw5-auth

---
+++ src/db/models/Contact.js
@@ -0,0 +1,56 @@
+import { Schema, model } from "mongoose";
+import { contactTypeList } from '../../constants/contacts.js';
+import { handleSaveError, setUpdateOptions } from './hooks.js';
+
+const contactSchema = new Schema({
+	name: {
+		type: String,
+		required: true,
+	},
+	phoneNumber: {
+		type: String,
+		required: true,
+
+	},
+	email: {
+		type: String,
+		required: false,
+
+	},
+	isFavourite: {
+		type: Boolean,
+		default: false,
+	},
+	contactType: {
+		type: String,
+		enum: contactTypeList,
+		required: true,
+		default: 'personal',
+	},
+	userId: {
+		type: Schema.Types.ObjectId,
+		ref: "user",
+		required: true,
+	}
+	// createdAt: {
+	// 	type: String,
+	// 	required: true,
+	// },
+	// updatedAt: {
+	// 	type: String,
+	// 	required: true,
+	// }
+
+}, { versionKey: false, timestamps: true });
+
+contactSchema.post('save', handleSaveError);
+
+contactSchema.pre('findOneAndUpdate', setUpdateOptions);
+
+contactSchema.post('findOneAndUpdate', handleSaveError);
+
+const ContactModel = model('contact', contactSchema);
+
+export const sortFields = ['name', 'phoneNumber', 'email', 'isFavourite', 'contactType', 'createdAt', 'updatedAt'];
+
+export default ContactModel;
